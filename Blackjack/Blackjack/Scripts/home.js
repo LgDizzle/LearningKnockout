a05111a1f058532e2c56044cc4357663ab229737
Lauren <Lauren@DESKTOP-HE2FG4V>
2017-02-04 15:04:03 -0600

    base commit

---
+++ Blackjack/Blackjack/Scripts/home.js
@@ -0,0 +1,8 @@
+﻿/// <reference path= "/scripts/knockout-3.4.1.debug.js" />
+
+$(function () {
+    var gameModel = {
+        gameName: ko.observable("Blackjack")
+    };
+    ko.applyBindings(gameModel);
+})
